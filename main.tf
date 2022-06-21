24626a4c5f14c256872b5dfd45b97187a5b884e8
Kirill Radzanovsky <Kirill.Radzanovskii@noveogroup.com>
2022-06-21 22:17:12 +0200

    Terraform fmt

--- main.tf
+++ main.tf
@@ -4,49 +4,49 @@ resource "kubernetes_namespace" "main" {
   }
 }
 resource "helm_release" "main" {
-  name       = var.project
+  name      = var.project
   namespace = kubernetes_namespace.main.metadata[0].name
-  chart      = "./helm/app"
+  chart     = "./helm/app"
 
   set {
-    name = "valuesChecksum"
-    value = filemd5("./helm/app/values.yaml") 
+    name  = "valuesChecksum"
+    value = filemd5("./helm/app/values.yaml")
   }
   set {
-    name = "image.repository"
+    name  = "image.repository"
     value = "ghcr.io/benc-uk/nodejs-demoapp"
   }
   set {
-    name = "image.tag"
+    name  = "image.tag"
     value = "latest"
   }
   set {
-    name = "env.TODO_MONGO_CONNSTR"
+    name  = "env.TODO_MONGO_CONNSTR"
     value = "mongodb://root:password@hubstaff-local-mongo.hubstaff-local.svc.cluster.local:27017/?retryWrites=false"
   }
   depends_on = [helm_release.mongo]
 }
 resource "helm_release" "mongo" {
   name       = "${var.project}-mongo"
-  namespace = kubernetes_namespace.main.metadata[0].name
+  namespace  = kubernetes_namespace.main.metadata[0].name
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "mongodb"
 
 
   set {
-    name = "nameOverride"
+    name  = "nameOverride"
     value = var.project
   }
   set {
-    name = "auth.rootUser"
+    name  = "auth.rootUser"
     value = "root"
   }
   set {
-    name = "auth.rootPassword"
+    name  = "auth.rootPassword"
     value = "password"
   }
   set {
-    name = "valuesChecksum"
-    value = filemd5("./helm/app/values.yaml") 
+    name  = "valuesChecksum"
+    value = filemd5("./helm/app/values.yaml")
   }
 }
